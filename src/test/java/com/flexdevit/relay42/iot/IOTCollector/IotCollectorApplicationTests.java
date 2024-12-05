3728b5e4ac06d165557d14ae27d41aba6a7ff1ec
Massimo Biancalani <massimo.biancalani@ing.com>
2024-12-05 16:07:06 +0100

    Initial commit

---
+++ src/test/java/com/flexdevit/relay42/iot/IOTCollector/IotCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexdevit.relay42.iot.IOTCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
